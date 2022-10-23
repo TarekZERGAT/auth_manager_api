99cd32ef317b722fd0973c7a6255e10fdb44b435
TarekZERGAT <t.zergat@gmail.com>
2022-10-23 20:41:57 +0100

    initial commit

---
+++ src/main/java/com/ztardz/auth_manager_api/AuthManagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.ztardz.auth_manager_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthManagerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthManagerApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ztardz/auth_manager_api/AuthManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ztardz.auth_manager_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthManagerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
